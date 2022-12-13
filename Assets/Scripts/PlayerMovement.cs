e7da29681315b2be63a3c48343514bdbf5bfa6cb
Leona <iosef1004@hotmail.com>
2022-12-13 10:36:39 -0500

    Added basic player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController controller;
+    private Vector3 velocity;
+
+    [SerializeField] float playerSpeed = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(-vertical, 0, horizontal);
+        controller.Move(move * Time.deltaTime * playerSpeed);
     }
 }
